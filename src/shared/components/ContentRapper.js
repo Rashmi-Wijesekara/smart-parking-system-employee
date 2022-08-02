e10ce7d79f0254561ee8dc989f2fc2e7ad05b4ce
kavishkamk <kavishkamadhushan727@gmail.com>
2022-08-03 01:31:24 +0530

    dashboard updated

--- src/shared/components/ContentRapper.js
+++ src/shared/components/ContentRapper.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContentRapper = (props) => {
     return (
-        <div className={`text-center relative w-full h-full-nav shadow-wrapper rounded-2xl mx-3 ${props.wrapperClasses}`}>
+        <div className={`text-center relative w-full h-full-nav shadow-wrapper overflow-y-scroll rounded-2xl mx-3 ${props.wrapperClasses}`}>
             {props.children}
         </div>
     );
